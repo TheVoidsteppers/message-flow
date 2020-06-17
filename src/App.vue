700fe3fd012dc3d31b405082b5ae2904e95e3c7f
TheVoidsteppers <15659974720@163.com>
2020-06-17 23:26:56 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <MessageFolw class="msg-box" :msgList="messageList" />
+  </div>
+</template>
+
+<script>
+import MessageFolw from "./components/MessageFolw";
+import "@/assets/style/index.css";
+
+export default {
+  name: "App",
+  components: {
+    MessageFolw
+  },
+  data() {
+    return {
+      messageList: [
+        {
+          type: "owner",
+          msg: "hello world",
+          msgType: "text",
+          avatar:
+            "http://www.520touxiang.com/uploads/allimg/202004111223/aph4ug1vd1r.jpg"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+/* http://www.520touxiang.com/uploads/allimg/202004111223/tre4xsmvup3.jpg */
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.msg-box {
+  margin: 0 auto;
+  width: 500px;
+  height: 400px;
+  border: 1px solid #000;
+}
+</style>
---
+++ src/components/MessageFolw.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="message-box">
+    <el-scrollbar>
+      <ul>
+        <li v-for="(item,index) in msgList" :key="index">
+          <img class="avatar" :src="item.avatar" alt />
+          <div class="msg-line">{{item.msg}}</div>
+        </li>
+      </ul>
+    </el-scrollbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MessageFolw",
+  props: {
+    msgList: {
+      type: Array,
+      default() {
+        return [];
+      }
+    }
+  },
+  methods: {}
+};
+</script>
+
+<style  scoped>
+.el-scrollbar {
+  height: 100%;
+}
+ul {
+  padding: 10px;
+}
+li {
+  display: flex;
+  flex-direction: column;
+}
+.avatar {
+  width: 20px;
+  height: 20px;
+}
+.msg-line {
+  position: relative;
+  background-color: #06c05f;
+}
+.msg-line::before {
+  width: 0;
+  height: 0;
+  content: "";
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  border-width: 10px;
+  border-style: solid;
+  border-color: transparent;
+  border-right-color: #fff;
+}
+</style>
